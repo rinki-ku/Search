0da4c8f8ff7260a96246a0cca9bb94e1fcae5959
Rinki Kumari <rinkikumari@rinkis-mbp.speedport.ip>
2019-11-06 15:39:05 +0100

    format on save

--- src/components/molecules/cardlist/index.js
+++ src/components/molecules/cardlist/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './style.css';
-import SearchCardTemplate from '../searchcardtemplate';
+import React from "react";
+import "./style.css";
+import SearchCardTemplate from "../searchcardtemplate";
 
 const cardList = ({ listItems }) => {
   return (
     <ul>
       {listItems.map((listItem, index) => {
-         const {id, name, address} = listItem
+        const { id, name, address } = listItem;
         return (
-            <SearchCardTemplate 
-                key={index}
-                id={id}
-                name={name}
-                address={address}
-            />
+          <SearchCardTemplate
+            key={index}
+            id={id}
+            name={name}
+            address={address}
+          />
         );
       })}
     </ul>
